04628acadd5639fee868eb1d50894bdb82105ea4
MiroGene <inkysun@163.com>
2017-07-25 17:47:18 +0800

    无版本变化。单位环境版

---
+++ Spider/src/main/java/com/setvar/SetVar.java
@@ -0,0 +1,9 @@
+package com.setvar;
+
+/**
+ * Created by Gene on 2017/7/25.
+ */
+public interface SetVar {
+
+    public void getVar(String varJson);
+}
---
+++ spiderweb/src/main/java/controller/NewsVar.java
@@ -0,0 +1,19 @@
+package controller;
+
+import com.setvar.SetVar;
+import com.vo.ConditionVo;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ * Created by Gene on 2017/7/25.
+ */
+public class NewsVar {
+
+    public String setSpiderVar(){
+        ConditionVo conditionVo = new ConditionVo();
+        //在此处转换为json
+
+        return "";
+    }
+}
